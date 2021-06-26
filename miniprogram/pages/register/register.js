5dadb762a901887450eaefc275751757a4daa765
CaptainLightningMeteor <1424673370@qq.com>
2021-06-26 21:57:23 +0800

    完善注释

--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -107,7 +107,7 @@ Page({
       this.id=userid;
       console.log("producing user");
     }
-    switch(role){
+    switch(role){//根据注册选择的账户类型创建用户对象
       case "USER":
         return new user({name:register_name,password:register_password,kind:"用户"});
         break;
@@ -126,7 +126,8 @@ Page({
         //console.log(register_name+' '+register_password+' '+confirm_password+' '+user_kind);
        if(register_name&&register_password&&confirm_password){
         for (let i = 0; i < user_register_info.length; i++) {  //遍历数据库对象集合
-          if (register_name === user_register_info[i].user_name) { //用户名存在
+          if (register_name === user_register_info[i].user_name) { 
+            //对比数据库里已有的用户名，若输入的用户名有重复则输出提示信息
             wx.showToast({
               title: '用户已存在！！',
               icon: 'none',
@@ -142,17 +143,18 @@ Page({
             })
           }
           else if(register_password.length<8||register_password.length>24){
+            //检查密码长度
             wx.showToast({
               title: '密码长度必须为8~24位！！',
               icon: 'none',
               duration: 500
             })
           }
-          else{
+          else{//若输入的信息符合要求则生成相应的用户对象存入数据库
             console.log("success "+register_name+' '+register_password+' '+confirm_password+' '+user_kind);
             let new_user=this.registerFactory(user_kind,user_register_info.length+1);
             console.log("factory has produced a new accout:"+new_user.name+' '+new_user.password+' '+new_user.kind+' '+new_user.id);
-            user_register.add({
+            user_register.add({//将注册的新用户添加到数据库中
               data:{
                 password:new_user.password,
                 user_id:new_user.id,
